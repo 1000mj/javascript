da603849ca32b463b646b4fe76b37ca209780fdb
1000mj <cjsaudwls12@gmail.com>
2023-05-15 20:48:17 +0900

    .length사용해서 확장성있는 코드로 만들기

--- tab.js
+++ tab.js
@@ -1,12 +1,12 @@
 const tabBtn = $('.tab-button');
 const tabContent = $('.tab-content');  
+const tabLength = $('.tab-button').length;
 
-
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < tabLength; i++){
     tabBtn.eq(i).on('click',function(){
-    tabBtn.removeClass('orange');
-    tabBtn.eq(i).addClass('orange');
-    tabContent.removeClass('show');
-    tabContent.eq(i).addClass('show');
+        tabBtn.removeClass('orange');
+        tabBtn.eq(i).addClass('orange');
+        tabContent.removeClass('show');
+        tabContent.eq(i).addClass('show');
     })
 }
