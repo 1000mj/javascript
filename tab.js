54481898fd8ce146922ec6dd291a2825843153a0
1000mj <cjsaudwls12@gmail.com>
2023-05-15 15:52:34 +0900

    for 반복문으로 탭기능 코드 줄이기

--- tab.js
+++ tab.js
@@ -1,23 +1,12 @@
 const tabBtn = $('.tab-button');
 const tabContent = $('.tab-content');  
 
-tabBtn.eq(0).on('click',function(){
-    tabBtn.removeClass('orange');
-    tabBtn.eq(0).addClass('orange');
-    tabContent.removeClass('show');
-    tabContent.eq(0).addClass('show');
-})
 
-tabBtn.eq(1).on('click',function(){
+for (let i = 0; i < 3; i++){
+    tabBtn.eq(i).on('click',function(){
     tabBtn.removeClass('orange');
-    tabBtn.eq(1).addClass('orange');
+    tabBtn.eq(i).addClass('orange');
     tabContent.removeClass('show');
-    tabContent.eq(1).addClass('show');
-})
-
-tabBtn.eq(2).on('click',function(){
-    tabBtn.removeClass('orange');
-    tabBtn.eq(2).addClass('orange');
-    tabContent.removeClass('show');
-    tabContent.eq(2).addClass('show');
-})
+    tabContent.eq(i).addClass('show');
+    })
+}
