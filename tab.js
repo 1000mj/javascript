e4b3d4fd1538a15129baa4680da93d513e74a931
1000mj <cjsaudwls12@gmail.com>
2023-05-16 20:42:52 +0900

    dataset으로 반복,조건문 안쓰고 해보기

--- tab.js
+++ tab.js
@@ -2,11 +2,25 @@ const tabBtn = $('.tab-button');
 const tabContent = $('.tab-content');  
 const tabLength = $('.tab-button').length;
 
-for (let i = 0; i < tabLength; i++){
-    tabBtn.eq(i).on('click',function(){
-        tabBtn.removeClass('orange');
-        tabBtn.eq(i).addClass('orange');
-        tabContent.removeClass('show');
-        tabContent.eq(i).addClass('show');
-    })
+// for (let i = 0; i < tabLength; i++){
+//     tabBtn.eq(i).on('click',function(){
+//         tabBtn.removeClass('orange');
+//         tabBtn.eq(i).addClass('orange');
+//         tabContent.removeClass('show');
+//         tabContent.eq(i).addClass('show');
+//     })
+// }
+
+function 탭열기(num){
+    tabBtn.removeClass('orange');
+    tabBtn.eq(num).addClass('orange');
+    tabContent.removeClass('show');
+    tabContent.eq(num).addClass('show');
 }
+
+
+$('.list').click(function(e){
+
+    탭열기(e.target.dataset.id);
+
+});
